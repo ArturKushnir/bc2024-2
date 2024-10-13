c6f99f7b1b41aaf019694a5e0178abedbf43c11b
ArturKushnir <artur.v.kushnir@gmail.com>
2024-10-13 20:59:36 +0300

    файл з кодом подібних завдань з курсу по js

---
+++ main.js
@@ -0,0 +1,11 @@
+// [
+//     { 
+//     "r030":392,"txt":"Єна","rate":0.25437,"cc":"JPY","exchangedate":"27.01.2024"
+//      }
+// ]
+
+//https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json&valcode=JPY&date=20240127
+
+fetch('https://isonplaceholder.typicode.com/users')
+.then (response => response. json())
+. then (value => console. log(value)) ;
